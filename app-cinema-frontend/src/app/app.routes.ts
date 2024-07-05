630976bb45d1661bc94b27bf01f7c0073b94d4f4
GambaX <m.gambardella05@gmail.com>
2024-07-05 13:48:05 +0200

    Removed route

--- app-cinema-frontend/src/app/app.routes.ts
+++ app-cinema-frontend/src/app/app.routes.ts
@@ -1,8 +1,6 @@
 import { Routes } from '@angular/router';
 import { FilmListComponent } from './film-list/film-list.component';
-import { ProgrammazioneFilmComponent } from './programmazione-film/programmazione-film.component';
 
 export const routes: Routes = [
-  { path: '', component: FilmListComponent },
-  { path: 'programmazionefilm/:id', component: ProgrammazioneFilmComponent },
+  { path: '', component: FilmListComponent }
 ];
